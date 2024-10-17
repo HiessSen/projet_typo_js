e731a4f1c7a4425ba2f2f56ab8ec2ed2801f8425
HiessSen <118723696+HiessSen@users.noreply.github.com>
2024-10-17 18:31:23 +0200

    1er jet. 23 mots, et 1 seul mode.

---
+++ script/test.js
@@ -0,0 +1,69 @@
+const listeMots = [
+  "bonjour",
+  "bien",
+  "présentation",
+  "accueil",
+  "élite",
+  "lithopedion",
+  "assassin",
+  "aspirateur",
+  "lieutenant",
+  "dimanche",
+  "apitoyer",
+  "canapé",
+  "ordinateur",
+  "bonnet",
+  "éponge",
+  "lunettes",
+  "écouteurs",
+  "stylo",
+  "dentier",
+  "pantalon",
+  "usine",
+  "aisselle",
+  "laboratoire"
+];
+
+const form = document.querySelector('#form')
+const affichageMot = document.querySelector('#affichageMot');
+const score = document.querySelector('#score');
+const word = document.querySelector('#word');
+const resultat = document.querySelector('#resultat');
+let motDonne = listeMots[Math.floor(Math.random() * listeMots.length)];
+affichageMot.innerHTML = motDonne;
+let scoreNumerique = 0;
+score.innerHTML = scoreNumerique;
+// resultat.innerHTML = "";
+console.log(scoreNumerique);
+
+function reponse(motEntreF, motDonneF){
+  if (motEntreF.toLowerCase() === motDonneF.toLowerCase()){
+    reussite(motEntreF, motDonneF);
+  }else{
+    echec(motEntreF, motDonneF);
+  }
+  motDonne = listeMots[Math.floor(Math.random() * listeMots.length)];
+  affichageMot.innerHTML = motDonne;
+}
+function reussite(){
+  scoreNumerique++;
+  score.innerHTML = scoreNumerique;
+  resultat.innerText = 'Bravo !';
+  console.log('Points : ' + scoreNumerique);
+  if(scoreNumerique==3){
+    victoire()
+  }
+}
+function echec(){
+  resultat.innerText = 'Essaye encore !';
+}
+function victoire(){
+  resultat.innerText = 'Vous avez réussi brillament ! N\'hésitez pas à rééssayer un niveau plus complexe.';
+  location.reload();
+}
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  let motEntre = word.value
+  reponse(motEntre, motDonne, scoreNumerique);
+  word.value = "";
+});
